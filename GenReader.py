c0acd8a7c208a7d3e06a8218bcdb36a2c1f27f5f
zenlunatik <zenlunatik64@gmail.com>
2014-09-25 20:57:55 -0700

    reader module with example output

--- GenReader.py
+++ GenReader.py
@@ -1,98 +1,95 @@
 class GenReader :
-	def genHeader(self, schemaDict) :
-		className=schemaDict['root']
-		namespace = className + 's'
-                namespace = namespace.title()
-                className = className.title() + "Reader"
-                headers = "#include \"helpers.h\"\n#include <string>\n#include <vector>\n\n"
-                namespaceBegin = "namespace " + namespace + "\n{\n"
-                classBegin = "     class " + className + "\n     {\n"
-                classDef = "     public:\n          virtual ~" + className + "() {};\n\n"
-                methodNames = ''
-                for key,value in schemaDict['data'].items() :
-			if key == schemaDict['root'] :
-				continue
-		        dataType = 'int'
-            		if value['type'] == 'int' :
-                		dataType = 'int'
-            		elif value['type'] == 'string' :
-                		dataType = 'std::string'
-            		elif value['type'] == 'stringvector' :
-                		dataType = 'std::vector<std::string>'
-                	methodNames = methodNames + "          virtual Status get" + key.title() + "(" + dataType + "& " + key + ") = 0;\n"
+     def genHeader(self, schemaDict) :
+        className=schemaDict['root']
+        namespace = className + 's'
+        namespace = namespace.title()
+        className = className.title() + "Reader"
+        headers = "#include \"helpers.h\"\n#include <string>\n#include <vector>\n\n"
+        namespaceBegin = "namespace " + namespace + "\n{\n"
+        classBegin = "     class " + className + "\n     {\n"
+        classDef = "     public:\n          virtual ~" + className + "() {};\n\n"
+        methodNames = ''
+        for key,value in schemaDict['data'].items() :
+            if key == schemaDict['root'] :
+                continue
+            dataType = "int"
+            if value['type'] == 'int' :
+                dataType = "int"
+            elif value['type'] == 'string' :
+                dataType = 'std::string'
+            elif value['type'] == 'stringvector' :
+                dataType = 'std::vector<std::string>'
+            methodNames = methodNames + "          virtual Status get" + key.title() + "(" + dataType + "& " + key + ") = 0;\n"
 
-                classEnd = "     }; // class " + className + "\n\n"
-                factory = "     class " + className + "Factory \n     {\n     public:\n          static " + className + "* createNew" + className + "(const char* " + className + "Id);\n     }; // class " + className + "Factory\n"
-        	namespaceEnd = "\n} // namespace " + namespace
-        	headerFO = open(className + ".h", "w")
-        	headerFO.write(headers)
-		headerFO.write(namespaceBegin)
-		headerFO.write(classBegin)
-                headerFO.write(classDef)
-                headerFO.write(methodNames)
-                headerFO.write(classEnd)    
-                headerFO.write(factory)
-                headerFO.write(namespaceEnd)
-                headerFO.close()
+        classEnd = "     }; // class " + className + "\n\n"
+        factory = "     class " + className + "Factory \n     {\n     public:\n          static " + className + "* createNew" + className + "(const char* " + className + "Id);\n     }; // class " + className + "Factory\n"
+        namespaceEnd = "\n} // namespace " + namespace
+        headerFO = open(className[0].lower() + className[1:] + ".h", "w")
+        headerFO.write(headers)
+        headerFO.write(namespaceBegin)
+        headerFO.write(classBegin)
+        headerFO.write(classDef)
+        headerFO.write(methodNames)
+        headerFO.write(classEnd)    
+        headerFO.write(factory)
+        headerFO.write(namespaceEnd)
+        headerFO.close()
 
-	def genSource(self, schemaDict) :
-        	className = schemaDict['root']
-        	namespace = className + 's'
-        	namespace = namespace.title()
-                className = className.title()
-                className = className + "Reader"
-        	headers = "#include \"" + className + "Update.h\"\n#include <boost/utility.hpp>\n\n"
-        	namespaceSet = "using namespace " + namespace + ";\n\n"
-        	comment = "//------------------" + className + "-------------\n"
-        	implClassName = className + "Impl"
-        	implClassNameSig = "class " + implClassName + " : public " + className + ",\n               private boost::noncopyable\n"
-        	constructor = "{\npublic:\n     " + implClassName + "(const char* " + className + "Id);\n"
-        	destructor = "     ~" + implClassName + "() {}\n\n"
-        	comment2 = "// ----------------BEGIN " + className + " interface --------------------------------\n"
-        	comment3 = "// ----------------END " + className + " interface -----------------------------------\n"
+     def genSource(self, schemaDict) :
+        className = schemaDict['root']
+        namespace = className + 's'
+        namespace = namespace.title()
+        className = className.title()
+        className = className + "Reader"
+        headers = "#include \"" + className + "Update.h\"\n#include <boost/utility.hpp>\n\n"
+        namespaceSet = "using namespace " + namespace + ";\n\n"
+        comment = "//------------------" + className + "-------------\n"
+        implClassName = className + "Impl"
+        implClassNameSig = "class " + implClassName + " : public " + className + ",\n               private boost::noncopyable\n"
+        constructor = "{\npublic:\n     " + implClassName + "(const char* " + className + "Id);\n"
+        destructor = "     ~" + implClassName + "() {}\n\n"
+        comment2 = "// ----------------BEGIN " + className + " interface --------------------------------\n"
+        comment3 = "// ----------------END " + className + " interface -----------------------------------\n"
         
-        	data = schemaDict['data']
-        	pvtVarNames =  "private:\n"
-        	rootTag = schemaDict['root']
-        	methodNames = ""
-        	methodDefs = ""
-        	for key,value in data.items() :
-            		if key == rootTag :
-	        		continue
-            		dataType = 'int'
-            		if value['type'] == 'int' :
-				dataType = 'int'
-	    		elif value['type'] == 'string' :
-                		dataType = 'std::string'
-            		elif value['type'] == 'vector' :
-                		dataType = 'std::vector<std::string>'
-                 
- 			pvtVarNames = pvtVarNames + "\n     " + dataType + " m_" + key
-           		methodNames = methodNames + "\n     Status get" + key.title() + "(" + dataType + " " + key + ");"
-            		methodDefs = methodDefs + "\nStatus\n" + implClassName + "::get" + key.title() + "(" + dataType + "&  " + key + ")\n{\n     return kSuccess\n}\n"
+        data = schemaDict['data']
+        pvtVarNames =  "private:\n"
+        rootTag = schemaDict['root']
+        methodNames = ""
+        methodDefs = ""
+        for key,value in data.items() :
+            if key == rootTag :
+                continue
+            dataType = 'int'
+            if value['type'] == 'int' :
+                dataType = 'int'
+            elif value['type'] == 'string' :
+                dataType = 'std::string'
+            elif value['type'] == 'vector' :
+                dataType = 'std::vector<std::string>'
+            
+            pvtVarNames = pvtVarNames + "\n     " + dataType + " m_" + key
+            methodNames = methodNames + "\n     Status get" + key.title() + "(" + dataType + "& " + key + ");"
+            methodDefs = methodDefs + "\nStatus\n" + implClassName + "::get" + key.title() + "(" + dataType + "&  " + key + ")\n{\n     return kSuccess\n}\n"
         
-        	methodNames = methodNames + "\n"
-        	classEnd = "\n}; // end " + implClassName + "\n\n"
-        	factoryMethod = "\n\n" + className + "*\n" + className + "Factory::createNew" + className + "(const char* " + className + "Id)\n{\n     return new " + implClassName + "(" + className + "Id);\n}"
+        methodNames = methodNames + "\n"
+        classEnd = "\n}; // end " + implClassName + "\n\n"
+        factoryMethod = "\n\n" + className + "*\n" + className + "Factory::createNew" + className + "(const char* " + className + "Id)\n{\n     return new " + implClassName + "(" + className + "Id);\n}"
         
-        	constImpl = implClassName + "::" + implClassName + "(const char* " + className + "Id) : " + "\n          " +  "\n{\n     //Set up defaults here\n}\n"
+        constImpl = implClassName + "::" + implClassName + "(const char* " + className + "Id) : " + "\n          " +  "\n{\n     //Set up defaults here\n}\n"
         
-        	srcFO = open( className + ".cc" , "w")
-                print className + ".cc"
-        	srcFO.write(headers)
-        	srcFO.write(namespaceSet)
-        	srcFO.write(comment)
-        	srcFO.write(implClassNameSig)
-        	srcFO.write(constructor)
-        	srcFO.write(destructor)
-        	srcFO.write(comment2)
-        	srcFO.write(methodNames)
-        	srcFO.write(comment3)
-        	srcFO.write(pvtVarNames)
-        	srcFO.write(classEnd)
-        	srcFO.write(constImpl)
-        	srcFO.write(methodDefs)
-        	srcFO.write(factoryMethod)
-        	srcFO.close()
-		
-
+        srcFO = open( className[0].lower() + className[1:] + ".cc" , "w")
+        srcFO.write(headers)
+        srcFO.write(namespaceSet)
+        srcFO.write(comment)
+        srcFO.write(implClassNameSig)
+        srcFO.write(constructor)
+        srcFO.write(destructor)
+        srcFO.write(comment2)
+        srcFO.write(methodNames)
+        srcFO.write(comment3)
+        srcFO.write(pvtVarNames)
+        srcFO.write(classEnd)
+        srcFO.write(constImpl)
+        srcFO.write(methodDefs)
+        srcFO.write(factoryMethod)
+        srcFO.close()
---
+++ examples/sessionReader.cc
@@ -0,0 +1,88 @@
+#include "SessionReaderUpdate.h"
+#include <boost/utility.hpp>
+
+using namespace Sessions;
+
+//------------------SessionReader-------------
+class SessionReaderImpl : public SessionReader,
+               private boost::noncopyable
+{
+public:
+     SessionReaderImpl(const char* SessionReaderId);
+     ~SessionReaderImpl() {}
+
+// ----------------BEGIN SessionReader interface --------------------------------
+
+     Status getUsername(std::string& username);
+     Status getPagesvisited(int& pagesVisited);
+     Status getIsmobileuser(int& isMobileUser);
+     Status getUserid(int& userid);
+     Status getLastaccesstime(int& lastAccessTime);
+     Status getUseragent(std::string& useragent);
+     Status getPassword(std::string& password);
+// ----------------END SessionReader interface -----------------------------------
+private:
+
+     std::string m_username
+     int m_pagesVisited
+     int m_isMobileUser
+     int m_userid
+     int m_lastAccessTime
+     std::string m_useragent
+     std::string m_password
+}; // end SessionReaderImpl
+
+SessionReaderImpl::SessionReaderImpl(const char* SessionReaderId) : 
+          
+{
+     //Set up defaults here
+}
+
+Status
+SessionReaderImpl::getUsername(std::string&  username)
+{
+     return kSuccess
+}
+
+Status
+SessionReaderImpl::getPagesvisited(int&  pagesVisited)
+{
+     return kSuccess
+}
+
+Status
+SessionReaderImpl::getIsmobileuser(int&  isMobileUser)
+{
+     return kSuccess
+}
+
+Status
+SessionReaderImpl::getUserid(int&  userid)
+{
+     return kSuccess
+}
+
+Status
+SessionReaderImpl::getLastaccesstime(int&  lastAccessTime)
+{
+     return kSuccess
+}
+
+Status
+SessionReaderImpl::getUseragent(std::string&  useragent)
+{
+     return kSuccess
+}
+
+Status
+SessionReaderImpl::getPassword(std::string&  password)
+{
+     return kSuccess
+}
+
+
+SessionReader*
+SessionReaderFactory::createNewSessionReader(const char* SessionReaderId)
+{
+     return new SessionReaderImpl(SessionReaderId);
+}
